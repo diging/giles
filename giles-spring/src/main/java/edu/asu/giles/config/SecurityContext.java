df322723a3ff739efc7551c1b197a1bcf489d6db
Julia Damerow <julia.damerow@googlemail.com>
2016-09-23 10:11:32 +0100

    [GIL-51] added property to allow iframing

--- giles-spring/src/main/java/edu/asu/giles/config/SecurityContext.java
+++ giles-spring/src/main/java/edu/asu/giles/config/SecurityContext.java
@@ -1,24 +1,40 @@
 package edu.asu.giles.config;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import org.springframework.security.web.header.writers.frameoptions.WhiteListedAllowFromStrategy;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter.XFrameOptionsMode;
 import org.springframework.social.security.SocialUserDetailsService;
 import org.springframework.social.security.SpringSocialConfigurer;
 
+import EDU.purdue.cs.bloat.benchmark.Stats;
+import edu.asu.giles.service.properties.IPropertiesManager;
+import edu.asu.giles.service.properties.impl.PropertiesManager;
 import edu.asu.giles.users.LocalUserDetailsService;
 import edu.asu.giles.users.SimpleSocialUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityContext extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    private IPropertiesManager propertiesManager;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -30,10 +46,31 @@ public class SecurityContext extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf()
+        HeadersConfigurer<HttpSecurity> config = http.csrf()
                 .disable()
-                // Configures form login
-                .formLogin()
+                .headers().frameOptions().sameOrigin();
+        
+        String iframeing = propertiesManager.getProperty(IPropertiesManager.ALLOW_IFRAMING_FROM);
+        if (iframeing == null) {
+            config.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsMode.DENY));
+        } else {
+            String[] origins = iframeing.split(",");
+            StringBuffer sb = new StringBuffer();
+            sb.append("frame-ancestors ");
+            
+            if (origins.length == 0) {
+                sb.append("'none'");
+            } else {
+                for (String org : origins) {
+                    sb.append(org);
+                    sb.append(" ");
+                }
+            }
+            config.addHeaderWriter(new StaticHeadersWriter("Content-Security-Policy", sb.toString()));
+        }
+        
+        // Configures form login
+        config.and().formLogin()
                 .loginPage("/")
                 .loginProcessingUrl("/login/authenticate")
                 .failureUrl("/?error=bad_credentials")
